1ef7b6b14af7dc40af570466c4186900c37a08e5
amongiove <amandamongiove@gmail.com>
2021-04-29 22:02:41 -0500

    [PET-6] implement type_selection route and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,26 @@
 class UsersController < ApplicationController
+    
+    def type_selection
+    end
 
+    def new
+    end
+
+    def create
+        puts "PARAMS"
+        puts params
+        # @user = User.new(user_params)
+        # if @user.save
+            # session[:user_id] = @user.id
+            # redirect_to @user
+        # else
+             render :new
+        # end
+    end
+
+    private 
+  
+    def user_params 
+      params.require(:user).permit(:name, :password, :email, :type, :street_address, :city, :state, :zip, :phone_number, :home_access_info, :bio, :rate)
+    end
 end
