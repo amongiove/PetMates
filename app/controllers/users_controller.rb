97e4a3fcb735fec6ac2d6744cc748fc7e99925f3
amongiove <amandamongiove@gmail.com>
2021-04-29 22:38:58 -0500

    [PET-6] create user_new form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,23 +4,23 @@ class UsersController < ApplicationController
     end
 
     def new
+        type = params["type"]
     end
 
     def create
-        puts "PARAMS"
-        puts params
+      
         # @user = User.new(user_params)
         # if @user.save
             # session[:user_id] = @user.id
             # redirect_to @user
         # else
-             render :new
+            #  render :new
         # end
     end
 
     private 
   
     def user_params 
-      params.require(:user).permit(:name, :password, :email, :type, :street_address, :city, :state, :zip, :phone_number, :home_access_info, :bio, :rate)
+      params.require(:user).permit(:name, :password, :email, :type, :street_address, :city, :state, :zip, :phone_number, :home_access_info, :bio, :rate, :verify_password)
     end
 end
