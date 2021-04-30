d3daa00efdff855f36f18a7a894ec61eeb6eb316
amongiove <amandamongiove@gmail.com>
2021-04-30 17:38:38 -0500

    [PET-7] create login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController 
     def new
+        puts "SESSIONS NEW"
+        render root_url
     end
 
     def create
+        puts "SESSIONS CREATE"
         user = User.find_by(name: params[:user][:name])
         @user = user.try(:authenticate, params[:password])
-        return redirect_to controller: 'users', action: 'new' unless @user
+        return redirect_to root_url unless @user
         session[:user_id] = user.id
         redirect_to onboarding_path(@user)    
     end
