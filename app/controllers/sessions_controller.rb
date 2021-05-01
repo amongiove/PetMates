82c6310e727c37dccb9eaf69baedc3a135d5d2e8
amongiove <amandamongiove@gmail.com>
2021-04-30 20:22:14 -0500

    [PET-12] implement logout route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,6 @@
 class SessionsController < ApplicationController 
-    # def new
-    #     puts "SESSIONS NEW"
-    #     render root_url
-    # end
-
+    
     def create
-        puts "SESSIONS CREATE"
         user = User.find_by(email: params[:user][:email])
         @user = user.try(:authenticate, params[:user][:password])
         return redirect_to controller: 'welcome', action: 'home' unless @user
@@ -13,9 +8,9 @@ class SessionsController < ApplicationController
         redirect_to user_path(@user)
     end
 
-    # def destroy
-    #     session.delete :user_id
-    #     redirect_to root_url
-    # end
+    def destroy
+        session.delete :user_id
+        redirect_to root_url
+    end
         
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root "welcome#home"#, to: "sessions#new"
-  
-  post '/login', to: "sessions#create"
+  root "welcome#home"
 
   get '/signup', to: "users#new"
   post '/signup', to: "users#create"
+
+  post '/login', to: "sessions#create"
+  #TODO: confirm -- delete isn't being routed to
+  get '/logout', to: "sessions#destroy"
   
   get '/users/(:id)', to: "users#edit"
 
-  # delete '/logout', to: "sessions#destroy"
+  
 
   resources :users
 
