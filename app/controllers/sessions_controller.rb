2c5decb2d2858a5ae69daaa2e80aa5c5bebc9a9e
amongiove <amandamongiove@gmail.com>
2021-04-30 20:05:13 -0500

    [PET-7] refactor sessions#create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,10 @@ class SessionsController < ApplicationController
     def create
         puts "SESSIONS CREATE"
         user = User.find_by(email: params[:user][:email])
-        @user = user.authenticate(params[:user][:password])
-        if @user
-            session[:user_id] = user.id
-            redirect_to user_path(@user)
-        else
-            redirect_to root_url
-        end    
+        @user = user.try(:authenticate, params[:user][:password])
+        return redirect_to controller: 'welcome', action: 'home' unless @user
+        session[:user_id] = user.id
+        redirect_to user_path(@user)
     end
 
     # def destroy
