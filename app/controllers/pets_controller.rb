8279ad0ba8e7bbcc39b790bea4f48cfd5d8c38ac
amongiove <amandamongiove@gmail.com>
2021-05-09 13:57:34 -0500

    [PET-8] implement create pet route

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,16 +10,22 @@ class PetsController < ApplicationController
 
     def create
         puts "CREATE"
+        @owner = User.find_by(id: session[:user_id])
         @pet = Pet.new(pet_params)
         if @pet.save
             #redirect broken
-            redirect_to owner_pet_path(@pet.id)
+            redirect_to owner_pet_path(@owner,@pet)
         else
             #TODO: fix flash messages so they are actual error messages
             render :new
         end
     end
 
+    def show
+        @pet = Pet.find_by(id: params[:id])
+    end
+
+    
     private 
   
     def pet_params 
