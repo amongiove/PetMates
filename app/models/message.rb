24f46ed56cbf90fdd18034cd10653a1d9c573637
amongiove <amandamongiove@gmail.com>
2021-05-11 17:20:07 -0500

    [PET-19] implement create messages and display message on page

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :job
+
+  def time_sent
+    time_sent = self.created_at
+    time_sent = time_sent.strftime("%b %e, %Y %l:%M %p")
+  end
 end
