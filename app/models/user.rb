9bfa1d4c972f66b84c938408621f18053fabba73
amongiove <amandamongiove@gmail.com>
2021-05-11 22:20:24 -0500

    [PET-21] generate reviews controller, resources, view folder

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
-    #has_many :jobs
     #has_many :reviews
     has_many :messages
     #before_create :set_user_type
--- config/routes.rb
+++ config/routes.rb
@@ -28,9 +28,10 @@ Rails.application.routes.draw do
   end
 
   resources :jobs do
-    resources :messages #only new? locks up
+    resources :messages #TODO: only new? -- locks up
   end 
 
+  resources :reviews
 
 end
 
